44cd74092497f284575a26ef92b46f280e233498
AndreGit00 <andre.j5samsung2016@gmail.com>
2024-08-31 13:45:30 +0200

    Correzione progetto
    Utilizzo di ant, correzione stringhe di connessione al database, iniziata la parte di aggiunta pulsanti di ricerca/inserimento/modifica/cancellazione dei dati.

---
+++ Libreria/src/main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Oliviero
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        TabellaDati tabellina;
+        tabellina = new TabellaDati();
+        tabellina.show();
+        
+        // TODO code application logic here
+    }
+    
+}
