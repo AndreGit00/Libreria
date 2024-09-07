8b2558531db22df4695c7ee103ccc514894fa98b
AndreGit00 <andre.j5samsung2016@gmail.com>
2024-09-07 22:32:36 +0200

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,33 @@
+
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+/**
+ *
+ * @author fedem
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        TabellaDati tabellina;
+        tabellina = new TabellaDati();
+        tabellina.show();
+
+        // Visualizzazione del programma a schermo intero
+        tabellina.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        // GraphicsDevice device = graphics.getDefaultScreenDevice();
+        // device.setFullScreenWindow(tabellina);
+        // ALTERNATIVA COMMENTATA
+
+        // TODO code application logic here
+    }
+
+}
