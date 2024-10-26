8358f5ed2d970f426808ac27149e2ae617f21dcd
AndreGit00 <andre.j5samsung2016@gmail.com>
2024-10-26 23:24:45 +0200

    Primo commit da NetBeans

--- src/BarraRicerca.java
+++ src/BarraRicerca.java
@@ -3,10 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 /**
  *
  * @author fedem
  */
+
+// Definizione della classe collegamento.
 public class BarraRicerca {
-    /** Inserire la classe barra*/
+    /**
+    
+    collegamento Connessione = new collegamento();
+    Statement st = Connessione.ottieniConnessione();
+    
+    public Statement Inserisci {
+    
+   
+}
+    
+    public Statement Modifica {
+    
+    
+}
+    
+    public Statement Elimina {
+    
+    
+} */
+            
 }
