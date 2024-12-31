895c6a7a5198f88d23dbaed2f4cd69ef7bbb5e64
AndreGit00 <andre.j5samsung2016@gmail.com>
2024-12-31 17:11:32 +0100

    Correzioni minori (formattazione).

--- src/FormattazioneVerticalizzata.java
+++ src/FormattazioneVerticalizzata.java
@@ -3,37 +3,30 @@ import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.*;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author fedem
+ */
 public class FormattazioneVerticalizzata implements TableCellRenderer {
 
-    // Costruttore della classe
     public FormattazioneVerticalizzata() {
-        // Non � necessario aggiungere nulla nel costruttore per questo caso
     }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        // Crea un JLabel per il rendering della cella
         JLabel label = new JLabel();
-
-        // Imposta il testo della cella
         label.setText(value == null ? "" : value.toString());
-
-        // Imposta lo sfondo della cella in base alla selezione
         label.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
-        label.setOpaque(true); // Imposta il JLabel come opaco per mostrare il colore di sfondo
-
-        // Allineamento orizzontale centrato
+        label.setOpaque(true);
         label.setHorizontalAlignment(JLabel.CENTER);
-
-        // Allineamento verticale centrato
         label.setVerticalAlignment(JLabel.CENTER);
-
-        // Se il testo contiene tag HTML (ad esempio <br> per le righe multiple), lo formatta
         if (label.getText().contains("<br>")) {
             label.setText("<html>" + label.getText().replaceAll("\n", "<br>") + "</html>");
         }
-
-        // Restituisce il JLabel come componente da rendere nella cella
         return label;
     }
 }
--- src/RecuperaArgomenti.java
+++ src/RecuperaArgomenti.java
@@ -2,30 +2,32 @@
 import java.sql.*;
 import java.util.ArrayList;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author fedem
+ */
 public class RecuperaArgomenti {
 
-    // Procedura per ottenere i valori della colonna 'Argomento' dalla tabella 'VittorioARGOMENTI'
     public static String[] OttieniArgomentiDaTabella() throws ClassNotFoundException {
-        // ArrayList per memorizzare i valori della colonna Argomento
         ArrayList<String> argomentiList = new ArrayList<>();
 
         try {
-            
             collegamento Connessione = new collegamento();
             Statement st = Connessione.ottieniConnessione();
             String sql = "select Argomento from VittorioARGOMENTI";
             ResultSet rs = st.executeQuery(sql);
 
-            // Aggiungi i valori alla lista
             while (rs.next()) {
                 String argomento = rs.getString("Argomento");
-                argomentiList.add(argomento); // Aggiungi ogni valore alla lista
+                argomentiList.add(argomento);
             }
 
-            // Converti la lista in un array
             String[] argomentiArray = argomentiList.toArray(new String[0]);
 
-            // Chiudi la connessione al database
             rs.close();
             st.close();
 
@@ -33,7 +35,7 @@ public class RecuperaArgomenti {
 
         } catch (SQLException e) {
             e.printStackTrace();
-            return new String[0]; // Restituisce un array vuoto in caso di errore
+            return new String[0];
         }
     }
 }
